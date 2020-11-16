30941b282b9b33373221534551e2d3cdc85a9b89
Nathaniel Schwartz <nathanielhschwartz@gmail.com>
2020-11-16 14:09:43 -0500

    completes jumpGame

--- jumpGame/jumpGame.js
+++ jumpGame/jumpGame.js
@@ -13,5 +13,44 @@
 // Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 var canJump = function(nums) {
+  if (nums.length === 1) {
+    return true;
+  }
 
+  var inaccessible = true;
+  for (var i = 0; i < nums.length - 1; i++) {
+    if ((nums.length - 1) - i <= nums[i]) {
+      inaccessible = false;
+      break;
+    }
+  }
+  if (inaccessible) {
+    return false;
+  }
+
+  var loserIdxCache = {};
+  var inProgress = {};
+
+  var recurse = function(idx) {
+    if (!nums[idx] || loserIdxCache[idx]) {
+      return false;
+    }
+    if ((nums.length - 1) - idx <= nums[idx]) {
+      return true;
+    }
+    inProgress[idx] = true;
+    for (var i = 1; i <= nums[idx]; i++) {
+      for (var stop of [idx - i, idx + i]) {
+        if (!inProgress[stop]) {
+          if (recurse(stop)) {
+            return true;
+          }
+        }
+        loserIdxCache[stop] = true;
+      }
+    }
+    return false;
+  };
+
+  return recurse(0);
 };
