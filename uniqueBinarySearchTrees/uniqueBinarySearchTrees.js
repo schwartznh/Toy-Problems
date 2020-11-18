016bedb77936c2cd8b5d8400d548d68f386f12c2
Nathaniel Schwartz <nathanielhschwartz@gmail.com>
2020-11-18 16:52:19 -0500

    completes uniqueBinarySearchTree

--- uniqueBinarySearchTrees/uniqueBinarySearchTrees.js
+++ uniqueBinarySearchTrees/uniqueBinarySearchTrees.js
@@ -14,5 +14,36 @@
 //    2     1         2                 3
 
 var numTrees = function(n) {
+  var treeCount = 0;
 
+  var factorial = function(n) {
+    if (!n) {
+      return 1;
+    }
+    var x = 1;
+    for (var i = 2; i <= n; i++) {
+      x *= i;
+    }
+    return x;
+  };
+
+  var choose = function(setSize, subsetSize) {
+    return (factorial(setSize) / (factorial(subsetSize) * factorial (setSize - subsetSize)));
+  };
+
+  var recurse = function(nodesOnLevel, nodesLeftToPlace, multiplier) {
+    if (nodesLeftToPlace === 0) {
+      treeCount += multiplier;
+    } else if (nodesLeftToPlace < 0 || nodesOnLevel === 0) {
+      return;
+    } else {
+      for (var nodesPlaced = 0; nodesPlaced <= 2 * nodesOnLevel; nodesPlaced++) {
+        var combos = choose(2 * nodesOnLevel, nodesPlaced);
+        recurse(nodesPlaced, nodesLeftToPlace - nodesPlaced, multiplier * combos);
+      }
+    }
+  };
+
+  recurse(1, n - 1, 1);
+  return treeCount;
 };
